1d7a1027a197c2b1f463602e64b469d8522994ca
Hudson <devhudsoncontact@gmail.com>
2023-08-14 09:40:52 -0300

    implementing poc

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,202 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_branch_sdk/flutter_branch_sdk.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // FlutterBranchSdk.validateSDKIntegration();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomeScreen();
+      },
+      routes: [
+        GoRoute(
+          path: 'users/:userId',
+          builder: (BuildContext context, GoRouterState state) {
+            return UserScreen(userId: state.pathParameters['userId']!);
+          },
+        ),
+        GoRoute(
+          path: 'organizations/:orgId',
+          builder: (BuildContext context, GoRouterState state) {
+            return OrganizationScreen(
+              id: state.pathParameters['orgId']!,
+              params: state.uri.queryParameters,
+            );
+          },
+          routes: [
+            GoRoute(
+              path: 'members/:memberId',
+              builder: (context, state) => MemberScreen(
+                id: state.pathParameters['memberId']!,
+              ),
+              routes: [
+                GoRoute(
+                  path: 'projects/:projectId',
+                  builder: (context, state) =>
+                      ProjectsScreen(id: state.pathParameters['projectId']!),
+                )
+              ],
+            ),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routerConfig: _router,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+/// The home screen
+class HomeScreen extends StatefulWidget {
+  /// Constructs a [HomeScreen]
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    FlutterBranchSdk.initSession().listen(
+      (data) {
+        print(data);
+        if (data['+clicked_branch_link'] == true &&
+            data[r'$deeplink_path'] != null) {
+          context.go(data[r'$deeplink_path']);
+        }
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: const Text('Home Screen')),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            ElevatedButton(
+              onPressed: () => context.go('/users/123'),
+              child: const Text('User'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: () => context.go('/organizations/1'),
+              child: const Text('Organizations'),
+            ),
+            ElevatedButton(
+              onPressed: () =>
+                  context.go('/organizations/2/members/4?filter=456'),
+              child: const Text('Organizations + members'),
+            ),
+            ElevatedButton(
+              onPressed: () =>
+                  context.go('/organizations/2/members/4/projects/mgmresorts'),
+              child: const Text('Organizations + members + projects'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class UserScreen extends StatelessWidget {
+  final String userId;
+
+  const UserScreen({super.key, required this.userId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('User Screen')),
+      body: Center(
+        child: Text('User: $userId'),
+      ),
+    );
+  }
+}
+
+class OrganizationScreen extends StatelessWidget {
+  const OrganizationScreen({super.key, required this.id, required this.params});
+
+  final String id;
+  final Map<String, String> params;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Organization Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Org: $id'),
+            ...params.entries.map((e) => Text('${e.key} - ${e.value}'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MemberScreen extends StatelessWidget {
+  const MemberScreen({super.key, required this.id});
+
+  final String id;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Member Screen')),
+      body: Center(child: Text('Member $id')),
+    );
+  }
+}
+
+class ProjectsScreen extends StatelessWidget {
+  const ProjectsScreen({super.key, required this.id});
+
+  final String id;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Projects Screen')),
+      body: Center(child: Text('Project $id')),
     );
   }
 }
